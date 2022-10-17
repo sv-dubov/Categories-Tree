5b9cfb0610f2bd39fa2c2ea725d5f3659a33568a
Sviatoslav <sv_dubov@ukr.net>
2022-10-17 18:50:10 +0300

    Edit build tree

--- classes/Test.php
+++ classes/Test.php
@@ -2,29 +2,35 @@
 
 class Test
 {
-    public static function getCategoriesTree($conn)
+    public function getCategoriesTree($conn)
     {
         $sql = "SELECT * FROM categories";
         $stmt = $conn->prepare($sql);
         $stmt->execute();
-        $result = $stmt->fetchAll();
         $resultArr = [];
-
-        for ($i = 1; $i <= 10; $i++) {
-            foreach ($result as $k => $value) {
-                if ($value['parent_id'] == $i) {
-                    $newParent = $value['categories_id'];
-                    foreach ($result as $k => $value) {
-                        if ($value['parent_id'] === $newParent) {
-                            $resultArr[$i][$newParent][$value['categories_id']] = $value['categories_id'];
-                        }
-                    }
-                    if (empty($resultArr[$i][$newParent])) {
-                        $resultArr[$i][$newParent] = $newParent;
-                    }
-                }
+        while ($row = ($stmt->fetch(PDO::FETCH_ASSOC))) {
+            if ($row['parent_id'] == 0) {
+                $resultArr[$row['categories_id']] = [];
+            } else {
+                self::buildTree($row['parent_id'], $resultArr, $row['categories_id']);
             }
         }
         return $resultArr;
+    }
+
+    public static function buildTree($parentId, &$arr, $val)
+    {
+        foreach ($arr as $key => &$value) {
+            if ($key == $parentId) {
+                if (!is_array($value))
+                    $value = array($val => $val);
+                else {
+                    $value[$val] = $val;
+                }
+                return;
+            } else if (is_array($value)) {
+                self::buildTree($parentId, $value, $val);
+            }
+        }
     }
 }
